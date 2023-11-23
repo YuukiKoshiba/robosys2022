33cc623e2638e9119f6795f24df0dadef47b4c82
Yuuki Koshiba <aoikyosei.y.k@gmail.com>
2023-11-23 14:37:20 +0900

    Support interger only calculation

---
+++ robot_system/class/list2.py
@@ -0,0 +1,4 @@
+list2 = ["a", "b", "c", "d", "e"]
+print(list2[0:3])
+print(list2[2:])
+print(list2[0::2])
---
+++ robot_system/class/var.py
@@ -0,0 +1,4 @@
+name = 'ueda'
+money = 5
+print(name + 'nosyozikinn:' + str(money) + 'enn')
+print("{}nosyozikinn: {}enn".format(name, money) )
