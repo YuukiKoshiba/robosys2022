c508656e4d1693d30162875bd6ac960db6c6869b
Yuuki Koshiba <aoikyosei.y.k@gmail.com>
2023-11-28 02:43:15 +0900

    Write copyright and licenses notice

--- robot_system/class/list2.py
+++ robot_system/class/list2.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Yuuki Koshiba
+# SPDX-License-Identifier: BSD-3-Clause
+
 list2 = ["a", "b", "c", "d", "e"]
 print(list2[0:3])
 print(list2[2:])
--- robot_system/class/var.py
+++ robot_system/class/var.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Yuuki Koshiba
+# SPDX-License-Identifier: BSD-3-Clause
+
 name = 'ueda'
 money = 5
 print(name + 'nosyozikinn:' + str(money) + 'enn')
